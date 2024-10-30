5b16d7ced6beacbd7e7960edc275c30563c6f4d2
yuridpaula19 <117127895+yuridpaula19@users.noreply.github.com>
2024-10-30 16:54:07 -0400

    task001

---
+++ exemplo02.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = "Yuri";
+    $sobrenome = "De Paula";
+    $idade = 21;
+    $profissao = "Estudante";
+
+    echo "Oi sou $profissao $nome $sobrenome. <br>";
+    echo "Voce possui atualmente $idade anos ";
+?>
